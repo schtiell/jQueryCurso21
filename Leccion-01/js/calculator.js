7ca5bfe4a80005df8a69f61b2526161c74c13eef
jsayagot <isc.jsayago@hotmail.com>
2023-04-11 18:00:17 -0600

    calculator

---
+++ Leccion-01/js/calculator.js
@@ -0,0 +1,92 @@
+
+//calculator using jQuery
+
+//Encapsulacion de jQuery
+(function (){
+
+    //jQuery definition
+    $(function (){
+        
+        updateScreen = function(displayValue){
+            var displayValue = displayValue.toString();
+            $('.screen').html(displayValue.substring(0,10));
+        }
+    
+        isNumber = function (value) {
+            return !isNaN(value);
+        }
+    
+        isOperator = function (value){
+            return value === '/' || value === '*' || value === '+' || value === '-';
+        }
+    
+        operate = function(a,b,operation){
+            a = parseFloat(a);
+            b = parseFloat(b);
+            console.log(a,b,operation)
+    
+            if (operation === '+') return a+b;
+            if (operation === '-') return a-b;
+            if (operation === '*') return a*b;
+            if (operation === '/') return a/b;
+    
+        }
+
+        let result = 0;
+        let prevEntry = 0;
+        let operation = null;
+        let currentEntry = '0';
+        updateScreen(result);
+
+        $(".button").on("click", function(evt){
+            var buttonPressed = $(this).html();
+            console.log(buttonPressed);
+
+            if (buttonPressed === "C"){
+                result = 0;
+                currentEntry = '0';
+    
+            }else if (buttonPressed === "CE"){
+                currentEntry = '0';
+    
+            }else if (buttonPressed === "back"){
+                currentEntry = currentEntry.substring(0,currentEntry.length-1);
+    
+            }else if (buttonPressed === "+/-"){
+                currentEntry *= -1;
+    
+            }else if (buttonPressed === "."){
+                currentEntry += '.';
+    
+            }else if (isNumber(buttonPressed)){
+                if (currentEntry === '0') currentEntry = buttonPressed;
+                else currentEntry = currentEntry + buttonPressed;
+    
+            }else if (isOperator(buttonPressed)){
+                prevEntry = parseFloat(currentEntry);
+                operation = buttonPressed;
+                currentEntry = "";
+                
+            }else if (buttonPressed === "%"){
+                currentEntry = currentEntry/100;
+    
+            }else if (buttonPressed === "sqrt"){
+                currentEntry = Math.sqrt(currentEntry);
+    
+            }else if (buttonPressed === "1/x"){
+                currentEntry = 1/currentEntry;
+    
+            }else if (buttonPressed === "pi"){
+                currentEntry = Math.PI;
+    
+            }else if (buttonPressed === "="){
+    
+                currentEntry = operate(prevEntry, currentEntry, operation);
+                operation = null;
+            }
+
+    
+            updateScreen(currentEntry);
+        });
+    });
+})();
